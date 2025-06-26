53cf80a61df9f0e5359feeec564595f8172e2de2
RAUNAK SINGH <rsing76@uic.edu>
2025-06-26 11:08:20 -0700

    using it to tests

--- jest/src/components/greet/greet.spec.tsx
+++ jest/src/components/greet/greet.spec.tsx
@@ -2,14 +2,14 @@ import {render, screen} from '@testing-library/react';
 import { Greet } from './greet';
 
 describe('Greet', () => {
-    test('Greet renders correctly', ()=> {
+    it('Greet renders correctly', ()=> {
     render (<Greet />)
     const textElement = screen.getByText('Hello')
     expect(textElement).toBeInTheDocument()
     })
 
     describe('Nested', () =>{
-            test('Greet renders with name', () =>{
+            it('Greet renders with name', () =>{
             render(<Greet name='Raunak'/>)
             const textElement = screen.getByText('Hello Raunak')
             expect(textElement).toBeInTheDocument()
