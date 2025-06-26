1d68a0976a95d11f24dd75cbb76d0f06fcc749d1
RAUNAK SINGH <rsing76@uic.edu>
2025-06-26 13:55:23 -0700

    using getByPlaceholder

--- jest/src/components/application/application.tsx
+++ jest/src/components/application/application.tsx
@@ -10,7 +10,7 @@ export const Application = () => {
       <form>
         <div>
           <label htmlFor="name">Name</label>
-          <input type="text" id="name"/>
+          <input type="text" id = "name" placeholder="fullName"/>
         </div>
         <div>
           <label htmlFor="bio">Bio</label>
