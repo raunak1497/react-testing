65e52e1672edb24493dd66212c2ac000b3871dea
RAUNAK SINGH <rsing76@uic.edu>
2025-06-27 19:06:16 -0700

    queryBy for missing elements

--- jest/src/App.tsx
+++ jest/src/App.tsx
@@ -3,7 +3,7 @@ import { Application } from './components/application/application'
 // import { CounterTwo } from './components/counter-two/CounterTwo'
 // import { Counter } from './components/counter/Counter'
 // import { MuiMode } from './components/mui/MuiMode'
-// import { Skills } from './components/skills/Skills'
+import { Skills } from './components/skills/Skills'
 // import { Users } from './components/users/Users'
 // import { AppProviders } from './providers/AppProviders'
 
@@ -11,9 +11,9 @@ function App() {
   return (
     // <AppProviders>
       <div className="App">
-        <Application />
-        {/* <Skills skills={['HTML', 'CSS']} />
-        <Counter />
+        {/* <Application /> */}
+        <Skills skills={['HTML', 'CSS']} />
+        {/* <Counter />
         <CounterTwo count={1} />
         <Users />
         <MuiMode /> */}
---
+++ jest/src/components/skills/Skills.test.tsx
@@ -0,0 +1,35 @@
+import { render, screen } from "@testing-library/react";
+import { Skills } from "./Skills";
+
+describe('Skills', ()=>{
+    const skills = ["HTML", "CSS", "JavaScript"]
+    
+    test('renders correctly', () => {
+        render(<Skills skills={skills} />)
+        const listElement  = screen.getByRole("list")
+        expect(listElement).toBeInTheDocument();
+
+    })
+
+    test('renders list of skills', ()=>{
+        render(<Skills skills={skills}/>)
+        const listItemElements = screen.getAllByRole('listitem')
+        expect(listItemElements).toHaveLength(skills.length);
+    });
+
+    test('renders Login Button', () => {
+        render(<Skills skills={skills} />)
+        const loginButton = screen.getByRole('button', {
+            name: "Login"
+        })
+        expect(loginButton).toBeInTheDocument()
+    })
+
+    test('start learning button is not rendered', () => {
+        render(<Skills skills={skills} />)
+        const learningButton = screen.queryByRole('button', {
+            name: "Start learning"
+        })
+        expect(learningButton).not.toBeInTheDocument()
+    })
+})
