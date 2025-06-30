7eb0fa1e718a54d08a6716ed73001ba8bf66ee3c
RAUNAK SINGH <rsing76@uic.edu>
2025-06-30 11:28:14 -0700

    handling mouse interactions

--- jest/src/components/counter/counter.test.tsx
+++ jest/src/components/counter/counter.test.tsx
@@ -1,5 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import { Counter } from "./counter";
+import userEvent from "@testing-library/user-event";
 
 describe('Counter', ()=>{
     test('renders correctly', ()=>{
@@ -17,6 +18,33 @@ describe('Counter', ()=>{
         render(<Counter />)
         const countElement = screen.getByRole('heading');
         expect(countElement).toHaveTextContent("0");     
+
+    })
+
+
+    test('renders count of 1 after clicking the increment button', async ()=>{
+        userEvent.setup()
+        render(<Counter />)
+         const incrementButton = screen.getByRole('button', {
+            name: 'Increment'
+        });
+        await userEvent.click(incrementButton)
+
+        const countElement = screen.getByRole('heading');
+        expect(countElement).toHaveTextContent("1");  
+
+    })
+
+    test('renders count of 2 after clicking the increment button twice', async ()=>{
+        userEvent.setup()
+        render(<Counter />)
+         const incrementButton = screen.getByRole('button', {
+            name: 'Increment'
+        });
+        await userEvent.click(incrementButton)
+        await userEvent.click(incrementButton)
+        const countElement = screen.getByRole('heading');
+        expect(countElement).toHaveTextContent("2");  
 
     })
 })
