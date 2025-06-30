70d8c36058304eb4f59512d23e4ea63a1862ccf7
RAUNAK SINGH <rsing76@uic.edu>
2025-06-30 12:26:45 -0700

    removed individual wrapper by adding custom render

--- jest/src/mui/mui-mode.test.tsx
+++ jest/src/mui/mui-mode.test.tsx
@@ -1,12 +1,10 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen } from "../test-utils";
 import { MuiMode } from "./mui-mode";
 import { AppProviders } from "../providers/AppProviders";
 
 describe('MuiMode', ()=> {
     test('renders text correctly', ()=> {
-        render(<MuiMode />, {
-            wrapper: AppProviders
-        })
+        render(<MuiMode />)
         const headingElement = screen.getByRole("heading");
         expect(headingElement).toHaveTextContent("dark mode");
     })
